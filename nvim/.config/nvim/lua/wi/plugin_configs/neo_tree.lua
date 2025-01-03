4bcef3cabc453378f7d09bcbeb514f7029a4ac25
Washington Irving <iamwashingyou@gmail.com>
2025-01-03 20:45:23 +0300

    unhide files in neotree

--- nvim/.config/nvim/lua/wi/plugin_configs/neo_tree.lua
+++ nvim/.config/nvim/lua/wi/plugin_configs/neo_tree.lua
@@ -1,2 +1,13 @@
 vim.keymap.set("n", "<leader>nb", ":Neotree toggle<CR>", {})
 vim.keymap.set("n", "<leader>nbr", ":Neotree reveal<CR>", {})
+
+require("neo-tree").setup({
+    window = {
+        width = 30,
+    },
+    filesystem = {
+        filtered_items = {
+            visible = true,
+        },
+    },
+})
