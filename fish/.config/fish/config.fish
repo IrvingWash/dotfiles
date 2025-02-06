27db48f36d0db3912bf6f347c4da357e9d1da3da
Washington Irving <iamwashingyou@gmail.com>
2025-02-06 20:57:09 +0300

    show user/permissions in ls alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,7 +13,7 @@ fish_vi_key_bindings
 fish_vi_cursor
 
 # Aliases
-alias ls='eza --color=always --icons=always --classify=always -l -a --group-directories-first --show-symlinks --modified --no-permissions --no-user'
+alias ls='eza --color=always --icons=always --classify=always -l -a --group-directories-first --show-symlinks --modified'
 alias tree='eza -T'
 alias ppg='cd ~/Documents/ppg'
 alias dot='cd ~/dotfiles'
