46720c310845fe9b7d7d93f7ebc22978735771f9
Washington Irving <iamwashingyou@gmail.com>
2025-05-29 00:13:24 +0300

    fix theme switcher

--- nvim/.config/nvim/lua/wi/core/colorscheme.lua
+++ nvim/.config/nvim/lua/wi/core/colorscheme.lua
@@ -2,7 +2,7 @@ vim.cmd("colorscheme catppuccin")
 
 local current_hour = tonumber(os.date("%H"))
 
-if current_hour > 20 then
+if current_hour >= 20 or current_hour <= 7 then
     vim.cmd("set background=dark")
 else
     vim.cmd("set background=light")
