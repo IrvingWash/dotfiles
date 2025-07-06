a70951a4c9cb431dbe194dc382c4617637ee5f71
Washington Irving <iamwashingyou@gmail.com>
2025-07-06 23:10:55 +0300

    format

--- sketchybar/.config/sketchybar/plugins/ip_address.sh
+++ sketchybar/.config/sketchybar/plugins/ip_address.sh
@@ -4,13 +4,13 @@ IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
 IS_VPN=$(scutil --nwi | grep -m1 'utun' | awk '{ print $1 }')
 
 if [[ $IS_VPN != "" ]]; then
-	ICON=󰒄
+    ICON=󰒄
 elif [[ $IP_ADDRESS != "" ]]; then
-	ICON=󰛳
+    ICON=󰛳
 else
-	ICON=󰲛
+    ICON=󰲛
 fi
 
 sketchybar --set $NAME \
-	icon=$ICON         \
-	label="$LABEL"
+    icon=$ICON         \
+    label="$LABEL"
