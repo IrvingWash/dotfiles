07f40720b92e4583918aafc41a739e5764e4d287
Washington Irving <iamwashingyou@gmail.com>
2025-07-07 02:03:56 +0300

    go full vile luxury

--- sketchybar/.config/sketchybar/plugins/mail.sh
+++ sketchybar/.config/sketchybar/plugins/mail.sh
@@ -14,10 +14,10 @@ if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
     elif [[ $LABEL =~ ^[0-9]+$ ]]; then
         ICON_COLOR="0xffed8796"
     else
-        ICON_COLOR="0xffffffff"
+        ICON_COLOR="0xff807251"
     fi
 else
-    ICON_COLOR="0xffffffff"
+    ICON_COLOR="0xff807251"
 fi
 
 sketchybar --set $NAME icon=$ICON label="${LABEL}" icon.color=${ICON_COLOR}
