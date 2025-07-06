1c981f8532e0da73498ef579a6bf3a729c80a5f3
Washington Irving <iamwashingyou@gmail.com>
2025-07-06 05:28:08 +0300

    add fish for osx

---
+++ fish_osx/.config/fish/config.fish
@@ -0,0 +1,22 @@
+# Disable greeting
+set fish_greeting
+
+# PATH
+fish_add_path /opt/homebrew/bin
+fish_add_path /home/irving_wash/jai/bin
+
+# Vim
+fish_vi_key_bindings
+fish_vi_cursor
+
+# Aliases
+alias ls='eza --color=always --icons=always --classify=always -l -a --group-directories-first --show-symlinks --modified'
+alias tree='eza -T'
+alias ppg='cd ~/Documents/ppg'
+alias dot='cd ~/dotfiles'
+alias lazy='lazygit'
+
+# Functions
+function repo
+    gh repo view $argv -w
+end
