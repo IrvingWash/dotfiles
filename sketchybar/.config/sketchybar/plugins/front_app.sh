6bf382c9b1f4c862dd05d03281eaff260ad3fe1f
Washington Irving <iamwashingyou@gmail.com>
2025-07-07 00:17:20 +0300

    add icons

--- sketchybar/.config/sketchybar/plugins/front_app.sh
+++ sketchybar/.config/sketchybar/plugins/front_app.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
 if [ "$SENDER" = "front_app_switched" ]; then
-    sketchybar --set "$NAME" label="$INFO"
+    case "$INFO" in
+        "Ghostty") ICON=;;
+        "Arc") ICON=󰾔;;
+        "Google Chrome") ICON=󰾔;;
+        "Telegram") ICON=;;
+        "Slack") ICON=;;
+        "Finder") ICON=󰀶;;
+        "IINA") ICON=󰃽;;
+        "Thunderbird") ICON=;;
+        "Code") ICON=;;
+        "Notes") ICON=;;
+        "Calendar") ICON=;;
+        "Reminders") ICON=󱖫;;
+        "Steam") ICON=;;
+        "System Settings") ICON=;;
+        "Outline") ICON=󰖂;;
+        "Music") ICON=󰎄;;
+        "App Store") ICON=;;
+    esac
+
+    sketchybar --set "$NAME" icon="$ICON" label="$INFO"
 fi
