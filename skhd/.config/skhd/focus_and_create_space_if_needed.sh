d3b2b73a99bc4493b8e8e511d2f6be9952582ba3
Washington Irving <iamwashingyou@gmail.com>
2025-07-06 03:24:22 +0300

    configure yabai and skhd

---
+++ skhd/.config/skhd/focus_and_create_space_if_needed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+spaces=$(yabai -m query --spaces)
+space_exists=$(echo "$spaces" | jq --argjson SPACE $1'.[] | select(.index == $SPACE)')
+
+if [ -n "$space_exists" ]; then
+    yabai -m space --focus $1
+else
+    current_space_count=$(echo "$spaces" | jq 'length')
+    spaces_to_create=$(($1 - current_space_count))
+
+    if (( spaces_to_create > 0 )); then
+        for ((i = 0; i < spaces_to_create; i++)); do
+            yabai -m space --create
+        done
+    fi
+
+    yabai -m space --focus $1
+fi
