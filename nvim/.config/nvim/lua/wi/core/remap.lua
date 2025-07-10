a4bf61876c0839611a903eecdbd3d43f30402a39
Washington Irving <iamwashingyou@gmail.com>
2025-07-11 01:26:43 +0300

    add command runner map

--- nvim/.config/nvim/lua/wi/core/remap.lua
+++ nvim/.config/nvim/lua/wi/core/remap.lua
@@ -1,1 +1,11 @@
 vim.g.mapleader = " "
+
+-- Run command and save output in scratch
+vim.keymap.set(
+    "n",
+    "<leader>c",
+    function()
+        vim.cmd("Scratch")
+        vim.api.nvim_feedkeys(":read! ", "n", false)
+    end, { silent = true }
+)
